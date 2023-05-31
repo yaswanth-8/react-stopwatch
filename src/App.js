37212fd8009d689b1ff8eef27f2be5ecc3b80ff4
yaswanth <ya8wanth@gmail.com>
2023-05-31 22:36:37 +0530

    Add stopwatch project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+const App = () => {
+  const [time, setTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+  const [isPaused, setIsPaused] = useState(false);
+
+  useEffect(() => {
+    let intervalId = null;
+
+    if (isRunning && !isPaused) {
+      intervalId = setInterval(() => {
+        setTime((prevTime) => prevTime + 1);
+      }, 1000);
+    }
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [isRunning, isPaused]);
+
+  const formatTime = (timeInSeconds) => {
+    const hours = Math.floor(timeInSeconds / 3600)
+      .toString()
+      .padStart(2, "0");
+    const minutes = Math.floor((timeInSeconds % 3600) / 60)
+      .toString()
+      .padStart(2, "0");
+    const seconds = (timeInSeconds % 60).toString().padStart(2, "0");
+    return `${hours}:${minutes}:${seconds}`;
+  };
+
+  const handleStartStop = () => {
+    if (isRunning) {
+      setIsRunning(false);
+      setIsPaused(false);
+    } else {
+      setIsRunning(true);
+    }
+  };
+
+  const handlePauseResume = () => {
+    setIsPaused((prevPaused) => !prevPaused);
+  };
+
+  const handleReset = () => {
+    setTime(0);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container">
+      <h1>{formatTime(time)}</h1>
+      <button
+        className={isRunning ? "stopButton" : "startButton"}
+        onClick={handleStartStop}
+      >
+        {isRunning ? "Stop" : "Start"}
+      </button>
+      {isRunning && (
+        <button
+          className={isPaused ? "startButton" : "stopButton"}
+          onClick={handlePauseResume}
         >
-          Learn React
-        </a>
-      </header>
+          {isPaused ? "Resume" : "Pause"}
+        </button>
+      )}
+      <button className="reset" onClick={handleReset}>
+        Reset
+      </button>
     </div>
   );
-}
+};
 
 export default App;
